1a6e6b06bb20b7648fbc7a9a742b9f66797c8e46
NITSAN <sanjay@nitsantech.com>
2025-06-24 14:50:30 +0530

    Update ext_emconf.php
    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,11 +2,11 @@
 
 /** @phpstan-ignore-next-line */
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Headless News',
-    'description' => 'This extension provides integration with news to output content from TYPO3 in JSON format.',
+    'title' => 'Headless News Integration for TYPO3',
+    'description' => 'Integrates the TYPO3 News system with headless output, allowing you to deliver news content in clean JSON format for frontend apps and frameworks.',
     'state' => 'stable',
-    'author' => 'Łukasz Uznański',
-    'author_email' => 'extensions@macopedia.com',
+    'author' => 'Team T3Planet',
+    'author_email' => 'info@t3planet.de',
     'category' => 'fe',
     'internal' => '',
     'version' => '13.0.0',
